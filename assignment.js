dadd8d002f00fd670f3df6e977702712cc3dc579
Nasif123 <nasif.reza@northsouth.edu>
2021-01-22 09:35:23 +0600

    first problem

---
+++ assignment.js
@@ -0,0 +1,10 @@
+function kilometerToMeter(km){
+    if(km < 0){
+        console.log("Distance can't be negative");
+    }
+    else{
+        var distance = km * 1000;
+    }
+    return distance;
+}
+
