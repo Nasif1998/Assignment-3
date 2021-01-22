0fb7e9d12600a7e3da87cc23ba1603dd6f883779
Nasif123 <nasif.reza@northsouth.edu>
2021-01-22 10:24:52 +0600

    third problem

--- assignment.js
+++ assignment.js
@@ -25,3 +25,31 @@ function budgetCalculator(noOfWatch, noOfPhone, noOfLaptop){
     }
 }
 
+
+
+function hotelCost(days){
+    var cost =0;
+    if(days<=0){
+        console.log("You weren't there even for a day");
+    }
+    else{
+        if(days <= 10){
+            cost = days * 100;
+        }
+        else if(days <= 20){
+            var firstTenDayCost = 10 * 100;
+            var remainingDays = days - 10;
+            var secondPartCost = remainingDays * 80;
+            cost = firstTenDayCost + secondPartCost;
+        }
+        else{
+            var firstTenDayCost = 10 * 100;
+            var secondPartCost = 10 * 80;
+            var remainingDays = days - 20;
+            var thirdPartCost = remainingDays * 50;
+            cost = firstTenDayCost + secondPartCost + thirdPartCost;
+        }
+        return cost;
+    }
+}
+
