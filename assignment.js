61ccfb093a3ce4a190015c515569cb121b82016e
Nasif123 <nasif.reza@northsouth.edu>
2021-01-22 09:53:16 +0600

    second problem

--- assignment.js
+++ assignment.js
@@ -8,3 +8,20 @@ function kilometerToMeter(km){
     return distance;
 }
 
+
+
+
+function budgetCalculator(noOfWatch, noOfPhone, noOfLaptop){
+    if(noOfWatch<0 || noOfPhone<0 || noOfLaptop<0){
+        console.log("Number of elements can't be negative");
+    }
+    else{
+        var totalPriceOfWatch = noOfWatch * 50;
+        var totalPriceOfPhone = noOfPhone * 100;
+        var totalPriceOfLaptop = noOfLaptop * 500;
+
+        var total = totalPriceOfWatch + totalPriceOfPhone + totalPriceOfLaptop;
+        return total;
+    }
+}
+
